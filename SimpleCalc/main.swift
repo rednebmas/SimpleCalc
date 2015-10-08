57f7581aaa63f68e3cc30021bad6d961e33d6f49
Sam Bender <rednebmas@gmail.com>
2015-10-08 10:43:05 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Sam Bender on 10/8/15.
+//  Copyright © 2015 Sam Bender. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
